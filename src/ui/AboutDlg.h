f256fe4290f929033b9b87f2b4bd3d902f7c0222
iOrange <sergey.kudlay@hotmail.com>
2019-10-03 22:15:09 -0400

    + added @SkyLoaderr to credits * bumped up MetroEX version

--- src/ui/AboutDlg.h
+++ src/ui/AboutDlg.h
@@ -58,41 +58,41 @@ namespace MetroEX {
             this->button1 = (gcnew System::Windows::Forms::Button());
             (cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
             this->SuspendLayout();
-            //
+            // 
             // pictureBox1
-            //
+            // 
             this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
             this->pictureBox1->Location = System::Drawing::Point(13, 13);
             this->pictureBox1->Name = L"pictureBox1";
             this->pictureBox1->Size = System::Drawing::Size(128, 128);
             this->pictureBox1->TabIndex = 0;
             this->pictureBox1->TabStop = false;
-            //
+            // 
             // richTextBox1
-            //
+            // 
             this->richTextBox1->BorderStyle = System::Windows::Forms::BorderStyle::None;
             this->richTextBox1->Location = System::Drawing::Point(148, 13);
             this->richTextBox1->Name = L"richTextBox1";
             this->richTextBox1->ReadOnly = true;
-            this->richTextBox1->Size = System::Drawing::Size(414, 128);
+            this->richTextBox1->Size = System::Drawing::Size(414, 155);
             this->richTextBox1->TabIndex = 1;
             this->richTextBox1->Text = L"";
-            //
+            // 
             // button1
-            //
-            this->button1->Location = System::Drawing::Point(486, 148);
+            // 
+            this->button1->Location = System::Drawing::Point(486, 174);
             this->button1->Name = L"button1";
             this->button1->Size = System::Drawing::Size(75, 23);
             this->button1->TabIndex = 2;
             this->button1->Text = L"OK";
             this->button1->UseVisualStyleBackColor = true;
             this->button1->Click += gcnew System::EventHandler(this, &AboutDlg::button1_Click);
-            //
+            // 
             // AboutDlg
-            //
+            // 
             this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
             this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-            this->ClientSize = System::Drawing::Size(575, 182);
+            this->ClientSize = System::Drawing::Size(575, 204);
             this->Controls->Add(this->button1);
             this->Controls->Add(this->richTextBox1);
             this->Controls->Add(this->pictureBox1);
@@ -116,11 +116,12 @@ namespace MetroEX {
         private: System::Void AboutDlg_Load(System::Object^  sender, System::EventArgs^  e) {
             String^ rtfText = LR"({\rtf1\ansi\ansicpg1251\deff0\nouicompat\deflang1049{\fonttbl{\f0\fnil\fcharset204 Segoe UI;}{\f1\fnil\fcharset0 Segoe UI;}{\f2\fnil\fcharset0 Calibri;}}
 {\colortbl ;\red0\green77\blue187;\red243\green164\blue71;}
-{\*\generator Riched20 10.0.17763}\viewkind4\uc1
-\pard\qc\cf1\b\f0\fs18 MetroEX\cf0\b0  \b\i\f1\lang1033 v0.45a\b0\i0\f0\lang1049\par
+{\*\generator Riched20 10.0.18362}\viewkind4\uc1 
+\pard\qc\cf1\b\f0\fs18 MetroEX\cf0\b0  \b\i\f1\lang1033 v0.45b\b0\i0\f0\lang1049\par
 \par
 Created by Sergii "\cf2\b iOrange\cf0\b0 " Kudlai\f1\lang1033  in \f0\lang1049 2019\line\line\f1\lang1033 Contributors:\line\i Abramcumner\line Slik\par
-Shoker\f2\fs22\lang9\par
+Shoker\par
+SkyLoader\f2\fs22\lang9\par
 }
  )";
 
